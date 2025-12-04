f13faffab35d9a0c9915b75f404673ae31912ee8
Martijn Jacobs <maerteijn@gmail.com>
2025-12-04 12:26:24 +0100

    Create a django app config

---
+++ src/sgbo_notebook_api/config.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SGBONotebookConfig(AppConfig):
+    name = "sgbo_notebook_api"
+    verbose_name = "SGBO Notebook API"
--- src/sgbo_notebook_api/settings/base.py
+++ src/sgbo_notebook_api/settings/base.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "sgbo_notebook_api",
+    "sgbo_notebook_api.config.SGBONotebookConfig",
 ]
 
 MIDDLEWARE = [
